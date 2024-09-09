a5cb6adc4412296e0e2c874e1231cb7616a360df
Leo Dresang <lrdresang@yahoo.com>
2024-09-09 13:59:31 -0500

    lober stuff

--- vengeful_lobster/Enimies/lobster.gd
+++ vengeful_lobster/Enimies/lobster.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 var entered : bool
-var speed : int = 10
+var speed : int = 1
 var direction : Vector2
 
 func _ready():
 	var screen_rect = get_viewport_rect()
-	print(screen_rect)
+	#print(screen_rect)
 	entered = false
 	#pick a direction to the entrance
-	var dist = screen_rect.get_center() - position
+	var dist:Vector2 = screen_rect.get_center() - position
 	#check if need to move horizontally or vertically
 	if abs(dist.x) > abs(dist.y):
 		# move horizontally
@@ -23,3 +23,4 @@ func _ready():
 func _physics_process(delta):
 	velocity = direction * speed
 	move_and_slide()
+	print(position)
