b29f0c0c08100d2b3b03c0a481d5cc293174d200
Leo Dresang <lrdresang@yahoo.com>
2024-09-09 14:15:58 -0500

    fixed main menu and added first tile

--- vengeful_lobster/Menu/play_button.gd
+++ vengeful_lobster/Menu/play_button.gd
@@ -5,6 +5,14 @@ extends AnimatedSprite2D
 ## The actual button object.
 @onready var button:Button = $Button
 
+func _process(delta):
+	if (button.is_hovered()):
+		scale.x = 16
+		scale.y = 16
+	else:
+		scale.x = 15
+		scale.y = 15
+
 func _on_button_pressed() -> void:
 	transition_manager.scene_to_load = SceneNavigationGlobals.game_level
 	transition_manager.scene_change()
