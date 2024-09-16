647c3087243813f6a7b312d6e16f82f4ec09a8ba
Leo Dresang <lrdresang@yahoo.com>
2024-09-16 13:19:11 -0500

    lobster update

--- vengeful_lobster/Enemies/lobster.gd
+++ vengeful_lobster/Enemies/lobster.gd
@@ -69,7 +69,7 @@ func _physics_process(delta):
 	direction = direction.normalized()
 	
 	velocity = velocity.lerp(direction * speed, acceleration * delta)
-	if(!dying):
+	if(!dying || !attacking):
 		move_and_slide()
 
 
