2de705ed4687a86f1eb1670111321c2eefe8d0bd
quinnT2369 <144701969+quinnT2369@users.noreply.github.com>
2024-09-17 23:40:04 -0500

    Sound Rebalance
    Rebalanced sound overall and added start sound.

--- vengeful_lobster/Menu/play_button.gd
+++ vengeful_lobster/Menu/play_button.gd
@@ -12,8 +12,10 @@ func _process(delta):
 	else:
 		scale.x = 15
 		scale.y = 15
+		
 
 func _on_button_pressed() -> void:
 	Globals.player_died = false
 	transition_manager.scene_to_load = SceneNavigationGlobals.game_level
 	transition_manager.scene_change()
+	$Select.play()
